26004a59d898af27232b3f4e9b0921b66c8c6555
george@moralis.gr <george@moralis.gr@9bc4f572-79d3-11de-a146-1f7e517e55b8>
2009-09-02 10:07:35 +0000

    code under gpl3 license

--- trunk/pcsp_debugger/src/common/types.h
+++ trunk/pcsp_debugger/src/common/types.h
@@ -1,10 +1,19 @@
 /*
- * types.h
- *
- *  Created on: 15 nov. 2008
- *      Author: hli
- */
+This file is part of pcsp.
 
+pcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef TYPES_H_
 #define TYPES_H_
 
