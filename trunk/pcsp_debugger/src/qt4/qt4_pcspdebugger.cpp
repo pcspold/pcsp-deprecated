d3127b832961a72c5e77cfeab62d77f34b6d102c
george@moralis.gr <george@moralis.gr@9bc4f572-79d3-11de-a146-1f7e517e55b8>
2009-09-26 14:07:42 +0000

    an almost working memory viewer

--- trunk/pcsp_debugger/src/qt4/qt4_pcspdebugger.cpp
+++ trunk/pcsp_debugger/src/qt4/qt4_pcspdebugger.cpp
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "types.h"
+#include "qt4_memorycursor.h"
 #include "qt4_pcspdebugger.h"
 #include "../debugger.h"
 
