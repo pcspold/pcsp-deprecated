f7769ad0df4020f75cb2ec17c5e1f38f224eb2f9
george@moralis.gr <george@moralis.gr@9bc4f572-79d3-11de-a146-1f7e517e55b8>
2009-10-13 11:24:24 +0000

    renamed pcsp_debugger to pcsp-dbg

---
+++ trunk/pcsp-dbg/src/debugger.h
@@ -0,0 +1,37 @@
+/*
+This file is part of pcsp.
+
+pcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef DEBUGGER_H_
+#define DEBUGGER_H_
+
+struct debugger_s;
+
+struct debugger_s
+{
+    static debugger_s &self();
+
+    debugger_s();
+
+    ~debugger_s();
+
+    void initialize();
+
+    void finalize();
+};
+
+extern debugger_s &debugger;
+
+#endif /* DEBUGGER_H_ */
---
+++ trunk/pcsp-dbg/src/qt4/qt4_pcspdebugger.h
@@ -0,0 +1,50 @@
+/*
+This file is part of pcsp.
+
+pcsp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pcsp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef PCSPDEBUGGER_H
+#define PCSPDEBUGGER_H
+
+#include <QtGui/QMainWindow>
+#include <QtGui>
+
+#include <QtNetwork>
+#include <qlocalsocket.h>
+
+
+#include "ui_qt4_pcspdebugger.h"
+
+class pcspdebugger : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	pcspdebugger(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~pcspdebugger();
+
+private:
+	Ui::pcspdebuggerClass ui;
+	QLocalSocket *socket;
+	
+
+public slots:
+	void onActionConnectClick();
+private slots:
+	void displayError(QLocalSocket::LocalSocketError socketError);
+	void onDataReceive();
+	void onConnect();
+};
+
+#endif // PCSPDEBUGGER_H
