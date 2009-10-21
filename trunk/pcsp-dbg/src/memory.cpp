874cba61c58657977a8aac91a2c12b6e8ede2326
george@moralis.gr <george@moralis.gr@9bc4f572-79d3-11de-a146-1f7e517e55b8>
2009-10-21 09:40:26 +0000

    added precompiled headers support

--- trunk/pcsp-dbg/src/memory.cpp
+++ trunk/pcsp-dbg/src/memory.cpp
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with pcsp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+#include "stdafx.h"
 #include <stdio.h>
 #include <string.h>
 
