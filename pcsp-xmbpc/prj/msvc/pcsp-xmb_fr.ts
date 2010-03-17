19428428fa1bcbcf74e24c068b6acc8868525af5
george@moralis.gr <george@moralis.gr@9bc4f572-79d3-11de-a146-1f7e517e55b8>
2010-03-17 21:08:31 +0000

    new redesign for pcsp-xmb , not working quite well yet

---
+++ pcsp-xmbpc/prj/msvc/pcsp-xmb_fr.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>PcspXmbUi</name>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="29"/>
+        <source>PCSP - XMB</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="88"/>
+        <source>Play</source>
+        <translation>Jouer</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="121"/>
+        <source>Game Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="563"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="568"/>
+        <source>Covers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.ui" line="573"/>
+        <source>Settings</source>
+        <translation type="unfinished">Paramètres</translation>
+    </message>
+    <message>
+        <source>Change UMD Location</source>
+        <translation type="obsolete">Changer le dossier des images UMD</translation>
+    </message>
+</context>
+<context>
+    <name>QPcspXmb</name>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="58"/>
+        <source>Settings</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <source>Change Games folder</source>
+        <translation type="obsolete">Changer le dossier des jeux
+ UMD</translation>
+    </message>
+    <message>
+        <source>Auto-rename Games</source>
+        <translation type="obsolete">Auto-renommer les jeux</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="59"/>
+        <source>Change UMD Games folder</source>
+        <translation>Changer le dossier des jeux
+ UMD</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="60"/>
+        <source>Auto-rename UMD Games</source>
+        <translation>Auto-renommer les jeux UMD</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="116"/>
+        <source>Cannot find an emulator to play %1!</source>
+        <translation>Impossible de trouver un émulateur pour jouer à %1 !</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="135"/>
+        <source>Select UMD Games folder</source>
+        <translation>Sélectionner un dossier des jeux UMD</translation>
+    </message>
+    <message>
+        <source>Change UMD images folder</source>
+        <translation type="obsolete">Changer le dossier des images UMD</translation>
+    </message>
+    <message>
+        <location filename="../../src/qt4/qpcspxmb.cpp" line="116"/>
+        <source>PCSP - XMB</source>
+        <translatorcomment>what else ?</translatorcomment>
+        <translation>PCSP-XMB</translation>
+    </message>
+</context>
+</TS>
